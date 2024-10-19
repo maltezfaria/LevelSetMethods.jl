4c4b9313ba83f57dc6fe644d73c846738dc6706b
maltezfaria <maltezfaria@gmail.com>
2024-10-19 17:48:47 +0200

    format

--- examples/reinitialization_example.jl
+++ examples/reinitialization_example.jl
@@ -3,33 +3,33 @@ using LevelSetMethods
 using LinearAlgebra
 using Plots
 
-nx,ny = 100,100
-x     = LinRange(-1,1,nx)
-y     = LinRange(-1,1,ny)
-hx,hy = step(x),step(y)
-grid  = CartesianGrid(x,y)
-bc    = PeriodicBC(2)
-ϕ     = LevelSet(grid,bc) do (x,y)
-    1.0
+nx, ny = 100, 100
+x      = LinRange(-1, 1, nx)
+y      = LinRange(-1, 1, ny)
+hx, hy = step(x), step(y)
+grid   = CartesianGrid(x, y)
+bc     = PeriodicBC(2)
+ϕ      = LevelSet(grid, bc) do (x, y)
+    return 1.0
 end
-add_circle!(ϕ, SVector(0.,0.), .9)
-remove_rectangle!(ϕ, SVector(0.,0.5), SVector(.5, 1.0))
+add_circle!(ϕ, SVector(0.0, 0.0), 0.9)
+remove_rectangle!(ϕ, SVector(0.0, 0.5), SVector(0.5, 1.0))
 # modify the level set function such that it is no longer
 # a signed distance function
-modifier = MeshField(grid) do (x,y)
-    cos(x*4)*sin(y*4)
+modifier = MeshField(grid) do (x, y)
+    return cos(x * 4) * sin(y * 4)
 end
-@. ϕ.vals = min(ϕ.vals,.5)^3.0*(abs(modifier.vals)+.01)
+@. ϕ.vals = min(ϕ.vals, 0.5)^3.0 * (abs(modifier.vals) + 0.01)
 
-term1  = ReinitializationTerm()
-terms  = (term1,)
+term1 = ReinitializationTerm()
+terms = (term1,)
 b = zero(ϕ)
 integrator = ForwardEuler(0.5)
-eq = LevelSetEquation(;terms,integrator,state=ϕ,t=0,buffer=b)
+eq = LevelSetEquation(; terms, integrator, state = ϕ, t = 0, buffer = b)
 
 # comparison between the level set before and after reinitialization
 # heatmap(eq,title="Before")
 # plot!(eq,color=:black,colorbar=true)
-integrate!(eq,1.0)
-heatmap(eq,title="After")
-plot!(eq,color=:black,colorbar=true)
+integrate!(eq, 1.0)
+heatmap(eq; title = "After")
+plot!(eq; color = :black, colorbar = true)
--- src/derivatives.jl
+++ src/derivatives.jl
@@ -4,7 +4,6 @@ struct Upwind <: SpatialScheme end
 
 struct WENO5 <: SpatialScheme end
 
-
 """
     D⁰(ϕ::MeshField,I,dim)
 
