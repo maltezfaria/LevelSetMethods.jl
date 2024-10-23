2006a1e6ee567ebb57f10bf281e957aa89e1b664
maltezfaria <maltezfaria@gmail.com>
2024-10-23 18:34:43 +0200

    fix tests

---
+++ src/utils.jl
@@ -0,0 +1,36 @@
+# helps to obtain classical shapes's signed distance function
+function CircleSignedDistance(m, center, r)
+    rsq = r * r
+    return map(x -> sum((x .- center) .^ 2) - rsq, m)
+end
+function RectangleSignedDistance(m, center, size)
+    sized2 = 0.5 * size
+    return map(x -> maximum(abs.(x .- center) - sized2), m)
+end
+
+# helpers to add geometric shapes on the a level set
+function add_circle!(ϕ, center, r)
+    circle = CircleSignedDistance(mesh(ϕ), center, r)
+    return union!(values(ϕ), circle)
+end
+function remove_circle!(ϕ, center, r)
+    circle = CircleSignedDistance(mesh(ϕ), center, r)
+    return difference!(values(ϕ), circle)
+end
+
+function add_rectangle!(ϕ, center, size)
+    rectangle = RectangleSignedDistance(mesh(ϕ), center, size)
+    return union!(values(ϕ), rectangle)
+end
+function remove_rectangle!(ϕ, center, size)
+    rectangle = RectangleSignedDistance(mesh(ϕ), center, size)
+    return difference!(values(ϕ), rectangle)
+end
+
+# helpers to merge or make the difference between two level set functions
+@inline function Base.union!(ϕ1, ϕ2)
+    @. ϕ1 = min(ϕ1, ϕ2)
+end
+@inline function difference!(ϕ1, ϕ2)
+    @. ϕ1 = -min(-ϕ1, ϕ2)
+end
--- test/test-meshes.jl
+++ test/test-meshes.jl
@@ -3,12 +3,8 @@ using LevelSetMethods
 
 @testset "Basic ops" begin
     nx, ny = 100, 50
-    x      = LinRange(-1, 1, nx)
-    y      = LinRange(0, 3, ny)
-    grid   = CartesianGrid(x, y)
-    @test size(grid) === (length(x), length(y))
-    @test meshsize(grid)[1] ≈ step(x)
-    @test meshsize(grid)[2] ≈ step(y)
-    @test grid[6, 2] == (x[6], y[2])
-    @test length(CartesianIndices(grid)) == length(x) * length(y)
+    a, b   = (-1, 0), (1, 3)
+    grid   = CartesianGrid(a, b, (nx, ny))
+    @test size(grid) === (nx, ny)
+    @test length(CartesianIndices(grid)) == nx * ny
 end
