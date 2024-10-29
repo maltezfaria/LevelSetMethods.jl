5d83fe2d2b12359700a4cca780c065e6021c5cc2
maltezfaria <maltezfaria@gmail.com>
2024-10-29 13:30:59 +0100

    document time stepping

--- docs/src/time-integrators.md
+++ docs/src/time-integrators.md
@@ -1,1 +1,13 @@
 # [Time integration](@id time-integrators)
+
+The following options are available for time integration:
+
+```@example
+using LevelSetMethods
+using InteractiveUtils # hide
+subtypes(LevelSetMethods.TimeIntegrator)
+```
+
+These are all explicit schemes, and therefore a sufficiently small time step, dependant on
+the `LevelSetTerm` being used, is required to ensure stability. We recommend using the third
+order [`RK3`](@ref) scheme for most applications.
--- src/timestepping.jl
+++ src/timestepping.jl
@@ -11,35 +11,24 @@ abstract type TimeIntegrator end
 end
 cfl(fe::ForwardEuler) = fe.cfl
 
+"""
+    struct RK2
+
+Second order total variation dimishing Runge-Kutta scheme, also known as Heun's
+predictor-corrector method.
+"""
 @kwdef struct RK2 <: TimeIntegrator
     cfl::Float64 = 0.5
 end
 cfl(rk2::RK2) = rk2.cfl
 
-@kwdef struct RKLM2 <: TimeIntegrator
-    cfl::Float64 = 0.5
-end
-cfl(rk2::RKLM2) = rk2.cfl
+"""
+    struct RK3
 
+Third order total variation dimishing Runge-Kutta scheme.
+"""
 @kwdef struct RK3 <: TimeIntegrator
     cfl::Float64 = 0.5
 end
 
 cfl(rk3::RK3) = rk3.cfl
-
-# function evolve!(ϕ,integ::RK2,terms,bc,t,Δtmax=Inf)
-#     α = integ.cfl
-#     buffer1,buffer2 = integ.buffers[1],integ.buffers[2]
-#     fill!(values(buffer1),0)
-#     fill!(values(buffer2),0)
-#     #
-#     buffer1, Δtˢ = compute_terms!(buffer1,terms,ϕ,bc)
-#     Δt = min(Δtmax,α*Δtˢ)
-#     axpy!(-Δt/2,buffer1.vals,ϕ.vals) # ϕ = ϕ - dt/2*buffer1
-#     #
-#     @. buffer1.vals = ϕ.vals + Δt * buffer1.vals
-#     buffer2, _   = compute_terms!(buffer2,terms,buffer1,bc)
-#     #
-#     axpy!(-Δt/2,buffer2.vals,ϕ.vals) # ϕ = ϕ - dt/2*buffer2
-#     return ϕ,t+Δt
-# end
