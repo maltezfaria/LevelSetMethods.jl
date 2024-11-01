f13dc00d2cefabde24ed68eaa2748b8603afc622
NLebbe <lebbe.nicolas@gmail.com>
2024-11-01 17:55:06 +0100

    Update signed distance function to sphere and add sphere/star initializations for levelsets.

--- src/utils.jl
+++ src/utils.jl
@@ -1,7 +1,6 @@
 # helps to obtain classical shapes's signed distance function
 function CircleSignedDistance(m, center, r)
-    rsq = r * r
-    return map(x -> sum((x .- center) .^ 2) - rsq, m)
+    return map(x -> sqrt(sum((x .- center) .^ 2)) - r, m)
 end
 function RectangleSignedDistance(m, center, size)
     sized2 = 0.5 * size
@@ -33,6 +32,28 @@ function dumbbell(grid; width = 1, height = 1 / 5, radius = 1 / 4)
     add_circle!(ϕ, SVector(width / 2, 0.0), radius)
     add_rectangle!(ϕ, SVector(0.0, 0.0), SVector(width, height))
     return ϕ
+end
+
+function sphere(
+    grid;
+    center = (grid.hc + grid.lc) / 2.0,
+    radius = minimum(grid.hc - grid.lc) / 4.0,
+)
+    ϕ = LevelSet(x -> 1.0, grid)
+    add_circle!(ϕ, center, radius)
+    return ϕ
+end
+
+function star(grid; radius = minimum(grid.hc - grid.lc) / 4.0, deformation = 0.25, n = 5.0)
+    N = dimension(grid)
+    if N != 2
+        throw(ArgumentError("star shape is only available in two dimensions"))
+    end
+    return LevelSet(grid) do (x, y)
+        norm = sqrt(x^2 + y^2)
+        θ = atan(y, x)
+        return norm - radius * (1.0 + deformation * cos(n * θ))
+    end
 end
 
 # helpers to merge or make the difference between two level set functions
