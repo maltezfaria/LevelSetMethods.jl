e2a4f8312214439086e08ba3c489bc23918357c6
maltezfaria <maltezfaria@gmail.com>
2025-10-23 22:46:01 +0200

    specify `eltype` of `CartesianGrid` for type stability

--- src/meshes.jl
+++ src/meshes.jl
@@ -63,6 +63,8 @@ end
 
 Base.getindex(g::CartesianGrid, I::Int...) = g[CartesianIndex(I...)]
 
+Base.eltype(g::CartesianGrid) = typeof(g.lc)
+
 function _getindex(g::CartesianGrid, I::CartesianIndex)
     N = dimension(g)
     @assert N == length(I)
